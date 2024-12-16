37054f924e4907f4f8a7a62203bc7bfeb7b9d7ef
tot <alessandro.tote@studenti.unimi.it>
2024-12-16 21:28:59 +0100

    fix

--- java/MoveZeroes.java
+++ java/MoveZeroes.java
@@ -2,17 +2,18 @@ package java;
 
 public class MoveZeroes {
 
-    public void moveZeroes(int[] nums) {
-        int left = 0;
+    public class Solution {
+        public void moveZeroes(int[] nums) {
+            int left = 0;
 
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] != 0) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                left++;
+            for (int right = 0; right < nums.length; right++) {
+                if (nums[right] != 0) {
+                    int temp = nums[right];
+                    nums[right] = nums[left];
+                    nums[left] = temp;
+                    left++;
+                }
             }
         }
     }
-
 }
